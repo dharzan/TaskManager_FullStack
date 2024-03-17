1fa1ed6db3435168afbb492b0773ac3e5d3b9f65
dharzan <dharzanbigmountain@gmail.com>
2024-03-17 11:45:40 -0400

    first commit

---
+++ Components/SideBar/SideBar.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import styled from 'styled-components';
+
+const SideBar = () => {
+  return (
+    <SideBarStyled>SideBar</SideBarStyled>
+  )
+}
+
+const SideBarStyled= styled.nav`
+`;
+
+export default SideBar
